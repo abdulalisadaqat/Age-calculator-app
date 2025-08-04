9025c7a466cc206bc225c524d05e69a809b2e05e
unknown <Ali@Github100M>
2025-08-04 16:39:21 +0430

    update commit

--- src/components/Output.jsx
+++ src/components/Output.jsx
@@ -3,7 +3,7 @@ const Output = ({ value, label }) => {
 		<>
 			<p className="text-[80px] leading-none font-extrabold italic mb-2 max-sm:text-6xl max-xs:text-5xl">
 				{value ? (
-					<span className="text-violet-500">{label}</span>
+					<span className="text-violet-500">{value}</span>
 				) : (
 					<span className="text-violet-500 tracking-[8px] ">--</span>
 				)}
